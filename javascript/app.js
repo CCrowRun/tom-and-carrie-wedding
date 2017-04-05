dc19e26a297654784b3767b72edced3bae26221b
CCrowRun <carrie.g.crow@gmail.com>
2017-04-05 14:36:56 -0500

    Testing app variable change
    Placed controller back in app.js

--- javascript/app.js
+++ javascript/app.js
@@ -3,6 +3,29 @@
 //Declare module for website application 
 angular.module('app', []);
 
+//Controller manages pages user views when clicking tabs
+angular.module('app').controller('PageController', ['$scope', '$window', '$timeout', function($scope, $window, $timeout)
+{
+	//Sets a default page (home)
+	this.page = 1;
+
+	//Identifies page matching anchor tag
+	this.isSet = function(checkPage) {
+		return this.page === checkPage;
+	};
+
+	//Changes page to match page defined for anchor tag
+	this.setPage = function(setPage) {
+		this.page = setPage;
+	};
+
+   $scope.$watch('this.page', function(newVal, oldVal) {
+      if(newVal.page == 2) {
+         $timeout(function() { angular.element($window).triggerHandler('resize'); }, 0);
+      }
+   }, true);
+}]);
+
 //Application page directives
 	angular.module('app').directive('homePage', function(){
 		return {
